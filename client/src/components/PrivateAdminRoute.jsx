eac2500b54745577c9f961a3681690b665bad85a
fares <132559795+faresboulahmi@users.noreply.github.com>
2024-04-09 20:08:43 +0100

    Delete client directory

--- client/src/components/PrivateAdminRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function PrivateAdminRoute() {
-  const { currentUser } = useSelector((state) => state.user);
-  return currentUser && currentUser.role === "admin" ||currentUser.role === "MainAdmin" ? <Outlet/> : <Navigate to='/'/>
- }
--- client/src/url.js
+++
@@ -1,2 +0,0 @@
-export const url = "https://vast-tan-python-hem.cyclic.app"
-
